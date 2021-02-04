dbcb1e45c835863057f65727c5c77b5ae6733890
goreleaserbot <goreleaser@carlosbecker.com>
2021-02-05 08:36:04 +0900

    Brew formula update for genlink version v0.1.2

--- Formula/genlink.rb
+++ Formula/genlink.rb
@@ -5,21 +5,21 @@
 class Genlink < Formula
   desc "Generates the URL link in various formats"
   homepage "https://github.com/michimani/genlink"
-  version "0.1.1"
+  version "0.1.2"
   license "MIT"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/michimani/genlink/releases/download/v0.1.1/genlink_0.1.1_darwin_x86_64.tar.gz"
-    sha256 "d2869aa94b13e0c4baed51515e0eeb836bd19d6c9f87797bb0d87bba2bf93a30"
+    url "https://github.com/michimani/genlink/releases/download/v0.1.2/genlink_0.1.2_darwin_x86_64.tar.gz"
+    sha256 "9f222d53fc83ceb171adbbbffbae70de1e72364e44be75d203d6f989929834ae"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/michimani/genlink/releases/download/v0.1.1/genlink_0.1.1_linux_x86_64.tar.gz"
-    sha256 "f3550734620ae4d3d1c8c96353bdd1f2ba364d283ad8ad6e1a51b4ce54dd11bc"
+    url "https://github.com/michimani/genlink/releases/download/v0.1.2/genlink_0.1.2_linux_x86_64.tar.gz"
+    sha256 "89ed428d42da25c9ba79ab60aecf1f319588e48ce82a3fd8d3428dc81c26b936"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/michimani/genlink/releases/download/v0.1.1/genlink_0.1.1_linux_arm64.tar.gz"
-    sha256 "ac5b2f1268ac51002069c395c2e3e6e906357552ea44aa127445cf8ff387385b"
+    url "https://github.com/michimani/genlink/releases/download/v0.1.2/genlink_0.1.2_linux_arm64.tar.gz"
+    sha256 "3a5cabb0092034368bb360284a6619c136b6218cea070bc016fef7a23bdb0fa7"
   end
 
   def install
